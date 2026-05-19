4188535c92cfbb6a9c0d5ce04bc88343328874a7
Aditya Kumar <adityakumar9074@gmail.com>
2026-05-19 22:04:43 +0530

    .

--- src/Arr_LeftRotateTheArrayByOne.java
+++ src/Arr_LeftRotateTheArrayByOne.java
@@ -9,7 +9,7 @@ public class Arr_LeftRotateTheArrayByOne {
     }
 
     public static void main(String[] args) {
-        int arr[] = {1,2,3,4,5,6};
+        int arr[] = {1,2,3,4,5};
         rotateTheArr(arr);
         for (int i = 0; i <arr.length ; i++) {
             System.out.print(arr[i]+" ");
