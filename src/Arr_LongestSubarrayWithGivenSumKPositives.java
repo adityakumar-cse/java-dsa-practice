09481c2b44c464454b3261ee403c17af1ae76f6d
Aditya Kumar <adityakumar9074@gmail.com>
2026-05-30 22:40:25 +0530

    .

---
+++ src/Arr_LongestSubarrayWithGivenSumKPositives.java
@@ -0,0 +1,21 @@
+public class Arr_LongestSubarrayWithGivenSumKPositives {
+    public static int longestSubarrayWithGivenSumK(int arr[], int k){
+        int maxLength = 0;
+        int count = 0;
+        for (int i = 0; i <arr.length; i++) {
+            int sum = 0;
+            for (int j = i; j <arr.length ; j++) {
+                sum = sum + arr[j];
+                if(sum == k){
+                  maxLength = Math.max(maxLength, j-i+1);
+                }
+            }
+        }
+        return  maxLength;
+    }
+    public static void main(String [] args){
+        int arr[] = {1,2,3,4,5,6,7};
+        int k = 6;
+        System.out.println(longestSubarrayWithGivenSumK(arr, k));
+    }
+}
