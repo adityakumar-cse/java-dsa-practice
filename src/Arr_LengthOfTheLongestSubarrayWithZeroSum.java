d39f65b188e3d5c136da3184f25b2a582876ea9d
Aditya Kumar <adityakumar9074@gmail.com>
2026-06-01 23:58:06 +0530

    .

--- src/Arr_LengthOfTheLongestSubarrayWithZeroSum.java
+++ src/Arr_LengthOfTheLongestSubarrayWithZeroSum.java
@@ -24,14 +24,26 @@
 
 //=======================Optimized Approach===================================
 
+import java.util.HashMap;
+
 public class Arr_LengthOfTheLongestSubarrayWithZeroSum {
     public static int lengthOfTheLongestSubarrayWithZeroSum(int arr[]){
         int n = arr.length;
         int maxLength = 0;
         int sum = 0;
-
-
-
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i <n; i++) {
+            sum+=arr[i];
+            if(sum == 0) {
+                maxLength = i + 1;
+            }
+                if (map.containsKey(sum)) {
+                maxLength = Math.max(maxLength, i - map.get(sum));
+            }
+            else {
+                map.put(sum, i);
+            }
+        }
         return maxLength;
     }
     public static void main (String [] args){
